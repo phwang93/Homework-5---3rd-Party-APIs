436cc1d522e3a58354f882c3a8f0f86804df3e7c
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-04-23 19:32:27 -0700

    Project DONE

--- assets/js/moment.js
+++ assets/js/moment.js
@@ -1,31 +1,31 @@
 // makeTimeblocks function
-function makeTimeblocks(hours, existingTodo = "")
+function makeTimeblocks(hour, existingTask = "") {
+    // var to set current time
+    var currentHour = new Date().getHours();
 
-// var to set current time
-var currentHour= new Date().getHours();
+    // additions functions for the time = past, present, or future
+    var pastPresentorFuture = "future";
+    if (currentHour > hour) pastPresentorFuture = "past";
+    if (currentHour === hour) pastPresentorFuture = "present";
 
-// additions functions for the time = past, present, or future
-var pastPresentorFuture = "future";
-if (currentHour > hour) pastPresentorFuture = "past";
-if (currentHour === hour) pastPresentorFuture = "present";
+    //saved todos from local storage function
+    var existingTask = localStorage.getItem(hour);
+    console.log(existingTask);
 
-//saved todos from local storage function
-var existingTodo = localStorage.getItem(hour);
-
-//prevent exisitingTodo to display "null"
-if (existingTodo === null) {
-    existingTodo() = "";
-
-//create timeblocks using bootstrap
-$("#container").append($(`
-<div class="row time-block">
+    //prevent exisitingTodo to display "null"
+    if (existingTask === null) {
+        existingTask = 'N/A';
+        }
+        //create timeblocks using bootstrap
+        $(".container").append($(`
+        <div class="row time-block">
         <div class="hour col-1">${hour}:00</div>
-        <textarea name="" id="${hour}" cols="30" rows="3" class="description col-9 ${pastPresentorFuture}">${existingTodo}</textarea>
+        <textarea name="" id="${hour}" cols="30" rows="3" class="description col-9 ${pastPresentorFuture}">${existingTask}</textarea>
         <button class="btn saveBtn col-2">Save</button>
-    </div>`));
+        </div>`));
 }
 //create visible timeblocks
-for (var i = 14; i < 28; i++){
+for (var i = 9; i < 22; i++) {
     makeTimeblocks(i);
 }
 
@@ -38,5 +38,19 @@ function displayTime(){
     setTimeout(displayTime, 1000);
 }
 
-displayTime;
+displayTime();
 
+//functions for the saveButton
+var saveButton= document.querySelectorAll(".saveBtn")
+
+//create eventlistener for saveBtn
+for (var i =0; i < saveButton.length; i++) {
+    saveButton[i].addEventListener("click", saveTask);
+}
+
+//save tasks to localstorage
+function saveTask(event) {
+    var taskDescription= event.target.parentNode.children[1].value;
+    var taskTime= event.target.parentNode.children[1].id;
+    localStorage.setItem(taskTime, taskDescription);
+}
